4a962264e87c85eef041cb5be4cd010c66367496
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-04-08 17:44:15 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,61 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const palos = ["diamond", "heart", "club", "spade"];
+const cartas = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let generateCard = () => {
+  let paloIndex = palos[Math.floor(Math.random() * palos.length)];
+  let cartasIndex = cartas[Math.floor(Math.random() * cartas.length)];
+
+  let paloHTML = "";
+  switch (paloIndex) {
+    case "diamond":
+      paloHTML = "♦";
+      break;
+    case "heart":
+      paloHTML = "♥";
+      break;
+    case "club":
+      paloHTML = "♣";
+      break;
+    default:
+      paloHTML = "♠";
+  }
+  let cardHTML = `
+  <div class="card ${paloIndex}"> 
+    <div class="topIcon">${paloHTML}</div>
+    <div class="number">${cartasIndex}</div>
+    <div class="bottomIcon">${paloHTML}</div>
+  </div>
+  `;
+  document.querySelector("#cardContainer").innerHTML = cardHTML;
+};
+
+let interval = () => {
+  clearInterval(interval);
+  let interval = setInterval(generateCard, 10000);
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document
+    .querySelector("#generateCard")
+    .addEventListener("click", () => generateCard());
+  generateCard();
+  interval();
 };
